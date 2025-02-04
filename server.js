cb189708cc46a298fd48ece8a8225af76ecea3bc
CATR27 <catr2777@gmail.com>
2025-02-04 13:57:59 -0600

    cambios en el serve.js
    cambios en el serve.js

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const { parse } = require("url");
 const next = require("next");
 const fs = require("fs");
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; // 🔥 Permitir certificados autofirmados
-
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
@@ -19,8 +17,8 @@ app.prepare().then(() => {
   https.createServer(httpsOptions, (req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
-  }).listen(3000, "0.0.0.0", (err) => {
+  }).listen(3000, "172.16.4.47", (err) => {
     if (err) throw err;
-    console.log("🚀 Servidor HTTPS corriendo en https://localhost:3000");
+    console.log("🚀 Servidor HTTPS corriendo en https://172.16.4.47:3000");
   });
 });
