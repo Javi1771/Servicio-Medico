d4ba5182b3d590f5e40d139402de282a196e10b4
Javi1771 <jl728122@gmail.com>
2025-04-11 13:43:33 -0600

    Update _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,8 +3,28 @@ import { FormularioProvider } from "../context/FormularioContext";
 import AuthGuard from "../components/AuthGuard";
 import Head from "next/head";
 import ErrorBoundary from "../components/ErrorBoundary";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    //* Manejador que se ejecuta al intentar recargar/cerrar la pestaña
+    const handleBeforeUnload = (event) => {
+      //* Para algunos navegadores, es necesario prevenir el evento
+      //* y asignar algo a event.returnValue (aunque ya no se usa texto personalizado).
+      event.preventDefault();
+      event.returnValue = "";
+      //* Esto indicará al navegador que muestre el cuadro de confirmación estándar
+    };
+
+    //* Suscribimos el evento
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    //* Al desmontar, quitamos el listener para evitar fugas de memoria
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
+
   return (
     <>
       <Head>
@@ -13,9 +33,9 @@ function MyApp({ Component, pageProps }) {
       </Head>
       <ErrorBoundary>
         <FormularioProvider>
-            <AuthGuard>
-              <Component {...pageProps} />
-            </AuthGuard>
+          <AuthGuard>
+            <Component {...pageProps} />
+          </AuthGuard>
         </FormularioProvider>
       </ErrorBoundary>
     </>
